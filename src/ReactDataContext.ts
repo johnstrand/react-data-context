cc7b818ac01f351e111fefb300f42e4a3703e471
John Strand <john.strand@dekiru.se>
2018-09-13 20:09:20 +0200

    Implemented most of the functionality

--- src/ReactDataContext.ts
+++ src/ReactDataContext.ts
@@ -1,32 +1,46 @@
 import * as React from "react";
 
 type Callback = (value: any) => void;
+type SubscriberCallback = { [key: string]: Callback; }
+type ContextCallback = { [key: string]: SubscriberCallback };
+type Render = (value: any) => JSX.Element;
 
 interface IConsumerState {
     value?: any;
 }
 
 interface IConsumerProps {
-    name: string;
-    render: (value: any) => JSX.Element;
+    context: string;
 }
 
 export const ReactDataContext = (() => {
-    const callbacks: { [key: string]: Callback[] } = {};
+    const callbacks: ContextCallback = {};
 
-    const registerCallback = (name: string, callback: Callback) => {
-        if (!callbacks[name]) {
-            callbacks[name] = [];
+    const registerCallback = (context: string, name: string, callback: Callback) => {
+        if (!callbacks[context]) {
+            callbacks[context] = {};
         }
-        callbacks[name].push(callback);
+        callbacks[context][name] = callback;
     }
 
-    const invokeCallback = (name: string, value: any) => {
-        if (!callbacks[name]) {
+    const deregisterCallback = (name: string) => {
+        Object.keys(callbacks).forEach(context => callbacks[context][name] = undefined);
+    }
+
+    const invokeCallback = (context: string, value: any) => {
+        if (!callbacks[context]) {
             return;
         }
-        callbacks[name].forEach(cb => cb(value));
+        Object.keys(callbacks[context]).forEach(name => {
+            const cb = callbacks[context][name];
+            if(!cb) {
+                return;
+            }
+            cb(value);
+        });
     }
+
+    const generateName = (): string => Math.random().toString(36).substring(7);
 
     return {
         Producer: class {
@@ -35,23 +49,35 @@ export const ReactDataContext = (() => {
             }
         },
         Service: {
-            update: (name: string, value: any) => {
-                invokeCallback(name, value);
+            update: (context: string, value: any) => {
+                invokeCallback(context, value);
             },
-            register: (name: string, callback: Callback) => {
-                registerCallback(name, callback);
+            register: (context: string, callback: Callback) => {
+                registerCallback(context, "__service", callback);
             }
         },
         Consumer: class extends React.Component<IConsumerProps, IConsumerState> {
+            __consumerName: string;
             constructor(props: IConsumerProps) {
                 super(props);
+                this.__consumerName = generateName();
                 this.state = { value: undefined };
-                registerCallback(props.name, value => this.setState({ value }));
+                registerCallback(props.context, this.__consumerName, value => this.setState({ value }));
             }
 
             public render() {
+                const render = this.props.children as Render;
+                if (!render || typeof render !== "function") {
+                    throw "Consumer must have single child of type function"
+                }
+
                 const { value } = this.state;
-                return this.props.render(value);
+
+                return render(value);
+            }
+
+            public componentWillUnmount() {
+                deregisterCallback(this.__consumerName);
             }
         }
     }
